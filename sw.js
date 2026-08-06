875939e2b5615e22d7ee590a61e639cc8d5c365c
ksw33584112-team <ksw33584112@gmail.com>
2026-08-06 23:17:46 +0900

    Update cache version to sowon-app-v15

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 소원물류 기사앱 서비스워커 (오프라인 캐시)
-const CACHE = "sowon-app-v14";
+const CACHE = "sowon-app-v15";
 const ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
